1f6f1ad724ff10c14377348da037fd21e6f7d22c
son.nguyen <nguyenson18296@gmail.com>
2024-02-10 17:57:55 +0700

    feat: pagination products

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -6,49 +6,66 @@ import { DisplayBar } from "./DisplayBar/DisplayBar";
 import { ProductsGridView } from "./ProductsGridView";
 import { ProductsListView } from "./ProductListView";
 import { useAppDispatch, useAppSelector } from "@/libs/hooks/useRedux";
-import { getProductsAction, getTotal, setIsLoading } from "@/libs/store/listProductsSlice";
+import {
+  getProductsAction,
+  getTotal,
+  setIsLoading,
+} from "@/libs/store/listProductsSlice";
 import { BASE_URL } from "@/constants";
 import { FilterItems } from "../common/FilterItem";
+import { Pagination } from "../common/Pagination/Pagination";
 
 export const ProductList: React.FC = () => {
-  // const [products, setProducts] = useState([]);
-  // const [isLoading, setIsLoading] = useState(true);
+  const [currenPage, setCurrentPage] = useState(1);
+  const [offset, setOffset] = useState(0);
+  const [limit, setLimit] = useState(10);
   const searchParams = useSearchParams();
   const dispatch = useAppDispatch();
-  const displayType = useAppSelector(state => state.productsSlice.displayType);
-  const products = useAppSelector(state => state.productsSlice.products);
-  const isLoading = useAppSelector(state => state.productsSlice.isLoading);
+  const { displayType, products, isLoading, total } = useAppSelector(
+    (state) => state.productsSlice
+  );
 
   const category_ids = searchParams.get("category_id");
 
   const getProducts = useCallback(async () => {
     dispatch(setIsLoading(true));
+    let query = `offset=${offset}&limit=${limit}`;
+    if (!!category_ids) {
+      query = query + `category_ids=${category_ids}`
+    }
+
     const response = await fetch(
-      `${BASE_URL}/products?${
-        !!category_ids ? `category_ids=${category_ids}` : ""
-      }`,
+      `${BASE_URL}/products?${query}`,
       {
         cache: "no-store",
       }
     );
     const data = await response.json();
-    dispatch(getProductsAction(data.data))
-    dispatch(getTotal(data.total))
-    dispatch(setIsLoading(false))
-  }, [category_ids, dispatch]);
+    dispatch(getProductsAction(data.data));
+    dispatch(getTotal(data.total));
+    dispatch(setIsLoading(false));
+  }, [category_ids, dispatch, offset, limit]);
 
   useEffect(() => {
     getProducts();
   }, [getProducts]);
 
   const renderProductsList = useCallback(() => {
-    if (displayType === 'grid') {
-      return (
-        <ProductsGridView products={products} isLoading={isLoading} />
-      )
+    if (displayType === "grid") {
+      return <ProductsGridView products={products} isLoading={isLoading} />;
     }
-    return <ProductsListView products={products} isLoading={isLoading} />
+    return <ProductsListView products={products} isLoading={isLoading} />;
   }, [displayType, isLoading, products]);
+
+  const onChangePage = useCallback((page: number) => {
+    setCurrentPage(page);
+    const offset = (page - 1) * limit;
+    setOffset(offset);
+  }, [limit]);
+
+  const onChangePerPage = useCallback((perPage: number) => {
+    setLimit(perPage)
+  }, []);
 
   return (
     <div>
@@ -57,6 +74,13 @@ export const ProductList: React.FC = () => {
       <div className="relative min-h-[400px] mt-4 w-full">
         {renderProductsList()}
       </div>
+      <Pagination
+        total={total}
+        currenPage={currenPage}
+        onChangePage={onChangePage}
+        onChangePerPage={onChangePerPage}
+        limit={limit}
+      />
     </div>
   );
 };
